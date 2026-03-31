fb81958bafb3f5553c6f7fa90ce9475060fadd5d
Lukejoanderson <lukejoanderson@gmail.com>
2026-03-31 00:15:17 -0500

    Added HSV, clamped RGB, first pass sort conditions.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 
 #include "Adafruit_APDS9960.h"
 Adafruit_APDS9960 apds;
-void reorgRGB(uint16_t rgb[]);
+void reorgRGB(int rgb[]);
+void RGB2HSV(int rgb[], double hsv[]);
 void setup() {
   Serial.begin(115200);
 
@@ -36,7 +37,7 @@ void loop() {
   Serial.print(" blue: ");
   Serial.print(b);
   Serial.println();
-  uint16_t RGB[3]={r,g,b};
+  int RGB[3]={r,g,b};
   reorgRGB(RGB);
     Serial.print("Fixed red: ");
   Serial.print(RGB[0]);
@@ -47,18 +48,78 @@ void loop() {
   Serial.print(" Fixed blue: ");
   Serial.print(RGB[2]);
   Serial.println();
-  /* Serial.print(" clear: ");
-  Serial.println(c);
-  Serial.println(); */
+  double HSV[3];
+  RGB2HSV(RGB,HSV);
+
+  Serial.print("Hue: ");
+  Serial.print(HSV[0]);
+  
+  Serial.print(" Shade: ");
+  Serial.print(HSV[1]);
+  
+  Serial.print(" Value: ");
+  Serial.print(HSV[2]);
+  Serial.println();
+if (HSV[0]>=70&&HSV[0]<=260&&HSV[1]>.25&&HSV[2]>.1)
+{
+  Serial.println("That's Recycling");
+}
+else
+{
+  Serial.println("That's Trash");
+}
   Serial.print("Closeness: ");
   Serial.println(apds.readProximity());
   delay(500);
 }
 
 
-void reorgRGB(uint16_t rgb[])
+void reorgRGB(int rgb[])
  {
     rgb[0]=rgb[0]*3.9584-46.958;
+    rgb[0]=min(max(rgb[0],0),255);
     rgb[1]=rgb[1]*2.0098-21.194;
+    rgb[1]=min(max(rgb[1],0),255);
     rgb[2]=rgb[2]*3.0622-195.16;
+    rgb[2]=min(max(rgb[2],0),255);
+}
+
+void RGB2HSV(int rgb[], double hsv[])
+{
+  //https://math.stackexchange.com/questions/556341/rgb-to-hsv-color-conversion-algorithm
+  double R=rgb[0];
+  double G=rgb[1];
+  double B=rgb[2];
+  R=R/255;
+  G=G/255;
+  B=B/255;
+  double cmax=max(max(R,G),B);
+  double cmin=min(min(R,G),B);
+  double delta=cmax-cmin;
+  if (R==G&&R==B)
+  {
+    hsv[0]=0;
+  }
+  else if (cmax==R)
+  {
+    hsv[0]=60*fmod((G-B)/delta,6);//main idea here is to turn negatives into positives that are at 300+ plus degrees, not really readable but whatever
+  }
+  else if (cmax==G)
+  {
+    hsv[1]=60*((B-R)/delta+2);
+  }
+  else
+  {
+    hsv[1]=60*((R-G)/delta+4);
+  }
+  if (cmax==0)
+  {
+    hsv[1]=0;
+  }
+  else
+  {
+    hsv[1]=delta/cmax;
+  }
+  hsv[2]=cmax;
+  
 }
